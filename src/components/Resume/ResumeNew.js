766fd1ade287f5b14df5cd46583d412d467f82c7
redhawc <redhawc1@gmail.com>
2023-06-11 15:15:04 +0800

    PDF download debug

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/haochen_CV.pdf";
+  "https://raw.githubusercontent.com/REDhawC/Portfolio/main/src/Assets/haochen_CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -30,7 +30,7 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download CV <br/>from  GitHub
           </Button>
         </Row>
 
